9c9eb05fefd98bf305c15ce6e5c76c88671d35f1
Kukks <evilkukka@gmail.com>
2019-04-01 16:32:31 +0200

    get ln channels action

---
+++ BtcTransmuter.Extension.Lightning/Actions/GetLightningChannels/GetLightningChannelsController.cs
@@ -0,0 +1,79 @@
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using BtcTransmuter.Abstractions.Actions;
+using BtcTransmuter.Abstractions.ExternalServices;
+using BtcTransmuter.Abstractions.Recipes;
+using BtcTransmuter.Data.Entities;
+using BtcTransmuter.Data.Models;
+using BtcTransmuter.Extension.Lightning.ExternalServices.LightningNode;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace BtcTransmuter.Extension.Lightning.Actions.GetLightningChannels
+{
+    [Route("lightning-plugin/actions/get-ln-channels")]
+    [Authorize]
+    public class GetLightningChannelsController : BaseActionController<GetLightningChannelsController.GetLightningChannelsViewModel, GetLightningChannelsData>
+    {
+        private readonly IExternalServiceManager _externalServiceManager;
+
+        public GetLightningChannelsController(IMemoryCache memoryCache, UserManager<User> userManager,
+            IRecipeManager recipeManager, IExternalServiceManager externalServiceManager) : base(memoryCache,
+            userManager, recipeManager)
+        {
+            _externalServiceManager = externalServiceManager;
+        }
+
+        protected override async Task<GetLightningChannelsViewModel> BuildViewModel(RecipeAction from)
+        {
+            var fromData = from.Get<GetLightningChannelsData>();
+            var services = await _externalServiceManager.GetExternalServicesData(new ExternalServicesDataQuery()
+            {
+                Type = new[] {LightningNodeService.LightningNodeServiceType},
+                UserId = GetUserId()
+            });
+            return new GetLightningChannelsViewModel
+            {
+                
+                RecipeId = from.RecipeId,
+                ExternalServiceId = from.ExternalServiceId,
+                ExternalServices = new SelectList(services, nameof(ExternalServiceData.Id),
+                    nameof(ExternalServiceData.Name), from.ExternalServiceId),
+            };
+        }
+
+        protected override async Task<(RecipeAction ToSave, GetLightningChannelsViewModel showViewModel)> BuildModel(
+            GetLightningChannelsViewModel viewModel, RecipeAction mainModel)
+        {
+            if (ModelState.IsValid)
+            {
+                mainModel.ExternalServiceId = viewModel.ExternalServiceId;
+                mainModel.Set<GetLightningChannelsData>(viewModel);
+                return (mainModel, null);
+            }
+
+            var services = await _externalServiceManager.GetExternalServicesData(new ExternalServicesDataQuery()
+            {
+                Type = new[] {LightningNodeService.LightningNodeServiceType},
+                UserId = GetUserId()
+            });
+
+
+            viewModel.ExternalServices = new SelectList(services, nameof(ExternalServiceData.Id),
+                nameof(ExternalServiceData.Name), viewModel.ExternalServiceId);
+            return (null, viewModel);
+        }
+
+        public class GetLightningChannelsViewModel : GetLightningChannelsData, IUseExternalService, IActionViewModel
+        {
+            public string RecipeId { get; set; }
+            public string RecipeActionIdInGroupBeforeThisOne { get; set; }
+            public SelectList ExternalServices { get; set; }
+            [Display(Name = "Lightning Node External Service")]
+            [Required] public string ExternalServiceId { get; set; }
+        }
+    }
+}
---
+++ BtcTransmuter.Extension.Lightning/Actions/GetLightningChannels/GetLightningChannelsDataActionHandler.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BtcTransmuter.Abstractions.Actions;
+using BtcTransmuter.Abstractions.Helpers;
+using BtcTransmuter.Data.Entities;
+using BtcTransmuter.Extension.Lightning.ExternalServices.LightningNode;
+using BtcTransmuter.Extension.NBXplorer.Services;
+using BTCPayServer.Lightning;
+using Microsoft.Extensions.DependencyInjection;
+using NBitcoin;
+
+namespace BtcTransmuter.Extension.Lightning.Actions.GetLightningChannels
+{
+    public class GetLightningChannelsDataActionHandler : BaseActionHandler<GetLightningChannelsData, LightningChannel[]>
+    {
+        public override string ActionId => "GetLightningChannels";
+        public override string Name => "Get Lightning Node Channels";
+
+        public override string Description =>
+            "get channels of a connected lightning network node";
+
+        public override string ViewPartial => "ViewGetLightningChannelsAction";
+
+        public override string ControllerName => "GetLightningChannels";
+
+        protected override async Task<TypedActionHandlerResult<LightningChannel[]>> Execute(
+            Dictionary<string, object> data, RecipeAction recipeAction,
+            GetLightningChannelsData actionData)
+        {
+            using (var serviceScope = DependencyHelper.ServiceScopeFactory.CreateScope())
+            {
+                var service = new LightningNodeService(recipeAction.ExternalService,
+                    serviceScope.ServiceProvider.GetService<NBXplorerClientProvider>(),
+                    serviceScope.ServiceProvider.GetService<NBXplorerSummaryProvider>(),
+                    serviceScope.ServiceProvider.GetService<SocketFactory>()
+                );
+
+                var client = service.ConstructClient();
+                var result = await client.ListChannels();
+                return new TypedActionHandlerResult<LightningChannel[]>()
+                {
+                    Executed = true,
+                    Result =
+                        $"Found {result.Length} channels",
+                    TypedData = result
+                };
+            }
+        }
+    }
+}
