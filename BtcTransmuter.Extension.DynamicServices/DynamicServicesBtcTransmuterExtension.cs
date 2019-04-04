033dbd84e6fceaaf4c9fe3c86e1e1ca613ba689d
Kukks <evilkukka@gmail.com>
2019-04-04 15:31:55 +0200

    add dynamic service extensions

---
+++ BtcTransmuter.Extension.DynamicServices/DynamicServicesBtcTransmuterExtension.cs
@@ -0,0 +1,9 @@
+using BtcTransmuter.Abstractions.Extensions;
+
+namespace BtcTransmuter.Extension.DynamicServices
+{
+    public class DynamicServicesBtcTransmuterExtension : BtcTransmuterExtension
+    {
+        public override string Name => "Dynamic Services Plugin";
+    }
+}
