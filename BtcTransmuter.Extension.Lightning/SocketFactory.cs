31a95c381501b4cac0060c50d7d11acd91a3ce0e
Kukks <evilkukka@gmail.com>
2019-04-05 10:38:04 +0200

    bump nbitcoin and reflact socketfactory changes from btcpay

--- BtcTransmuter.Extension.Lightning/SocketFactory.cs
+++ BtcTransmuter.Extension.Lightning/SocketFactory.cs
@@ -6,6 +6,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using NBitcoin;
+using NBitcoin.Protocol;
+using NBitcoin.Protocol.Connectors;
 
 namespace BtcTransmuter.Extension.Lightning
 {
@@ -16,7 +18,7 @@ namespace BtcTransmuter.Extension.Lightning
         private EndPoint GetSocksEndpoint()
         {
             var socksEndpointString = _configuration.GetValue<string>("socksendpoint", null);
-            if(!string.IsNullOrEmpty(socksEndpointString))
+            if (!string.IsNullOrEmpty(socksEndpointString))
             {
                 if (!Utils.TryParseEndpoint(socksEndpointString, 9050, out var endpoint))
                     throw new Exception("Invalid value for socksendpoint");
@@ -25,84 +27,54 @@ namespace BtcTransmuter.Extension.Lightning
 
             return null;
         }
-        
-        public SocketFactory(IConfiguration  configuration)
+
+        public SocketFactory(IConfiguration configuration)
         {
             _configuration = configuration;
         }
-      public async Task<Socket> ConnectAsync(EndPoint endPoint, CancellationToken cancellationToken)
+
+        public async Task<Socket> ConnectAsync(EndPoint endPoint, CancellationToken cancellationToken)
         {
-            Socket socket = null;
+            DefaultEndpointConnector connector = new DefaultEndpointConnector();
+            NodeConnectionParameters connectionParameters = new NodeConnectionParameters();
+            var endpoint = GetSocksEndpoint();
+            if (endpoint != null)
+            {
+                connectionParameters.TemplateBehaviors.Add(new NBitcoin.Protocol.Behaviors.SocksSettingsBehavior()
+                {
+                    SocksEndpoint = endpoint
+                });
+            }
+
+            var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
             try
             {
-                if (endPoint is IPEndPoint ipEndpoint)
-                {
-                    socket = new Socket(ipEndpoint.Address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-                    await socket.ConnectAsync(ipEndpoint).WithCancellation(cancellationToken);
-                }
-                else if (IsTor(endPoint))
-                {
-                    var endpoint = GetSocksEndpoint();
-                    if (endpoint == null)
-                        throw new NotSupportedException("It is impossible to connect to an onion address without btcpay's -socksendpoint configured");
-                    if (endpoint.AddressFamily != AddressFamily.Unspecified)
-                    {
-                        socket = new Socket(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-                    }
-                    else
-                    {
-                        // If the socket is a DnsEndpoint, we allow either ipv6 or ipv4
-                        socket = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
-                        socket.DualMode = true;
-                    }
-                    await socket.ConnectAsync(endpoint).WithCancellation(cancellationToken);
-                    await NBitcoin.Socks.SocksHelper.Handshake(socket, endPoint, cancellationToken);
-                }
-                else if (endPoint is DnsEndPoint dnsEndPoint)
-                {
-                    var address = (await Dns.GetHostAddressesAsync(dnsEndPoint.Host)).FirstOrDefault();
-                    socket = new Socket(address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-                    await socket.ConnectAsync(dnsEndPoint).WithCancellation(cancellationToken);
-                }
-                else
-                    throw new NotSupportedException("Endpoint type not supported");
+                await connector.ConnectSocket(socket, endPoint, connectionParameters, cancellationToken);
             }
             catch
             {
-                CloseSocket(ref socket);
-                throw;
+                SafeCloseSocket(socket);
             }
+
             return socket;
         }
 
-        private bool IsTor(EndPoint endPoint)
+        internal static void SafeCloseSocket(System.Net.Sockets.Socket socket)
         {
-            if (endPoint is IPEndPoint)
-                return endPoint.AsOnionDNSEndpoint() != null;
-            if (endPoint is DnsEndPoint dns)
-                return dns.Host.EndsWith(".onion", StringComparison.OrdinalIgnoreCase);
-            return false;
-        }
-
-        private void CloseSocket(ref Socket s)
-        {
-            if (s == null)
-                return;
             try
             {
-                s.Shutdown(SocketShutdown.Both);
+                socket.Shutdown(SocketShutdown.Both);
             }
             catch
             {
-                try
-                {
-                    s.Dispose();
-                }
-                catch { }
             }
-            finally
+
+            try
             {
-                s = null;
+                socket.Dispose();
+            }
+            catch
+            {
             }
         }
     }
