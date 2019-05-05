104c2074dfaec8fb13c82ad67ad8c389632e82e2
Kukks <evilkukka@gmail.com>
2019-05-05 19:15:43 +0200

    work on adding funcs to interpolator

--- BtcTransmuter.Abstractions/Helpers/InterpolationHelper.cs
+++ BtcTransmuter.Abstractions/Helpers/InterpolationHelper.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+
 namespace BtcTransmuter.Abstractions.Helpers
 {
     public class InterpolationHelper
@@ -15,16 +17,32 @@ namespace BtcTransmuter.Abstractions.Helpers
         {
             try
             {
+            
+                var parameterExpressions =
+                    data.Select(pair => Expression.Parameter(pair.Value.GetType(), pair.Key)).ToList();
+             
                 return Regex.Replace(value, @"{{(.+?)}}",
                     match =>
                     {
-                        var parameterExpressions =
-                            data.Select(pair => Expression.Parameter(pair.Value.GetType(), pair.Key)).ToArray();
-                        var e = System.Linq.Dynamic.DynamicExpression.ParseLambda(parameterExpressions, null,
-                            match.Groups[1].Value);
-                        
+                        var processed =  JsonFunc(match.Groups[1].Value, data);
+                        var e = System.Linq.Dynamic.DynamicExpression.ParseLambda(parameterExpressions.ToArray(), null,
+                            processed);
+                      
                         return (e.Compile().DynamicInvoke(data.Values.ToArray()) ?? "").ToString();
                     });
+            }
+            catch (Exception)
+            {
+                return value;
+            }
+        }
+
+        private static string JsonFunc(string value, Dictionary<string, object> data)
+        {
+            try
+            {
+                return Regex.Replace(value, @"ToJson\({1}(.+?)\){1}",
+                    match => JsonConvert.SerializeObject(InterpolateString("{{"+match.Groups[1].Value+"}}", data)));
             }
             catch (Exception)
             {
