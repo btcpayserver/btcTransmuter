0100da72f0b3138726527828bfcc456cc8cafd4e
Kukks <evilkukka@gmail.com>
2019-05-05 15:45:21 +0200

    bump nbitcoin and nbx

--- BtcTransmuter.Extension.BtcPayServer.Tests/GetInvoiceDataActionHandlerTests.cs
+++ BtcTransmuter.Extension.BtcPayServer.Tests/GetInvoiceDataActionHandlerTests.cs
@@ -1,11 +1,15 @@
+using System.Threading.Tasks;
 using BtcTransmuter.Extension.BtcPayServer.Actions.GetInvoice;
 using BtcTransmuter.Extension.BtcPayServer.HostedServices;
 using BtcTransmuter.Tests.Base;
+using Xunit;
 
 namespace BtcTransmuter.Extension.BtcPayServer.Tests
 {
     public class
         GetInvoiceDataActionHandlerTests : BaseActionTest<GetInvoiceDataActionHandler, GetInvoiceData, BtcPayInvoice>
     {
+       
+
     }
 }
