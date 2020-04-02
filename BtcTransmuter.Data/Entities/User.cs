8ad099bcce7dc37eaf338dc8fffc9c563ed90ea0
Kukks <evilkukka@gmail.com>
2020-04-02 10:51:10 +0200

    Add Blob to User

--- BtcTransmuter.Data/Entities/User.cs
+++ BtcTransmuter.Data/Entities/User.cs
@@ -1,10 +1,14 @@
 using System.Collections.Generic;
+using BtcTransmuter.Data.Encryption;
+using BtcTransmuter.Data.Models;
 using Microsoft.AspNetCore.Identity;
 
 namespace BtcTransmuter.Data.Entities
 {
-    public class User : IdentityUser
+    public class User : IdentityUser, IHasJsonData
     {
         public List<Recipe> Recipes { get; set; }
+
+        [Encrypted] public string DataJson { get; set; }
     }
 }
---
+++ BtcTransmuter.Services/Migrations/20200402084229_AddUserBlob.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BtcTransmuter.Data.Migrations
+{
+    public partial class AddUserBlob : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DataJson",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataJson",
+                table: "AspNetUsers");
+        }
+    }
+}
